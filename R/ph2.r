00b779be5a156fda39e94ef9bc0620b4d9644184
mdeveaux <michelle.deveaux@yale.edu>
2016-11-16 13:39:09 -0500

    Started updating descriptions

--- R/ph2.r
+++ R/ph2.r
@@ -1,37 +1,52 @@
-#' Find critical values (r1, r2)
+#' Find critical values (r1, r2) 
+#' 
+#' Finds r1, the minimum number of Stage 1 successes to continue to 
+#' Stage 2, and r2, the minimum number of Stage 2 successes to 
+#' reject the null hypothesis.
+#' 
+#' Find r1 so that the probability of early stopping
+#' is less than or equal to pearly. 
+#' Find r2 from Binomial model with no early stopping
+#' and significance level alpha.
 #'
-#' DESCRIPTION HERE
-#'
-#' @param n n
-#' @param p p
-#' @param pearly pearly
-#' @param alpha alpha
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'          and Stage 2 (n2).
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2).
+#' @param pearly desired probability of early stopping (default = .1).
+#' @param alpha desired significance level (default = .1).
 #' @examples
-#' # Put exmple code here.
+#' ph2crit( n = c( 5, 31), p = c(.8,.2), pearly = .1, alpha = .1)
 #' @export
 ph2crit = function(n, p, pearly = .1, alpha = .1) {}
 
 #' Stopped negative binomial distribution mass function
 #'
-#' DESCRIPTION HERE
+#' Returns the probability density function for minimum number of 
+#' events for either s successes or t failures.
 #'
-#' @param p p
-#' @param s s
-#' @param t t
+#' @param p probability of success in each trial. 
+#' @param s number of successes.
+#' @param t number of failures.
 #' @examples
-#' # Put exmple code here.
+#' ph2dspb(p = .8, s = 3, t = 5)
 #' @export
 ph2dspb = function(p, s, t) {}
 
 #' Probability of stopping early 
 #'
-#' DESCRIPTION HERE
+#' Probability of stopping the trial after Stage 1
 #'
-#' @param p p
-#' @param n n
-#' @param r r
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2) or a scalar containing p1
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'          and Stage 2 (n2) or a scalar containing n1
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2) or a scalar containing r2
 #' @examples
-#' # Put exmple code here.
+#' ph2early(p = .8, n = 25, r = 18)
+#' ph2early(p = c(.8, .2), n = c(25, 9), r = 18)
 #' @export
 ph2early = function( p, n, r) {}
 
