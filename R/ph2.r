350ef5f32f1a0e06a6e306365bbd0229d9863564
mdeveaux <michelle.deveaux@yale.edu>
2016-12-12 15:45:28 -0500

    Finished function and parameter descriptions

--- R/ph2.r
+++ R/ph2.r
@@ -50,61 +50,83 @@ ph2dspb = function(p, s, t) {}
 #' @export
 ph2early = function( p, n, r) {}
 
-#' Expected sample size for decision to continue to Stage 2
+#' Expected sample size and SD for decision to continue to Stage 2
 #'
-#' DESCRIPTION HERE
+#' Mean and SD of the minimum number of Stage 1 patients necessary to be able
+#' to decide whether to either continue to Stage 2 or else terminate early
 #'
-#' @param p
-#' @param n1
-#' @param r1
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2) or a scalar containing p1
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'           and Stage 2 (n2) or a scalar containing n1
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2) or a scalar containing r1
 #' @examples
-#' # Put exmple code here.
+#' ph2Eearly( p = .8, n = 5, r = 3)
+#' ph2early(p = c(.8, .2), n = c(5, 31), r = 3)
 #' @export
 ph2Eearly = function(p, n1, r1) {}
 
 #' Expected curtailed sample size
 #'
-#' DESCRIPTION HERE
+#' The expected number of patients who are enrolled and followed to their
+#' endpoint before critical endpoints in Stage 1 and Stage 2 are achieved
 #'
-#' @param p p
-#' @param n n
-#' @param r r
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2) 
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'           and Stage 2 (n2) 
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2)
 #' @examples
-#' # Put exmple code here.
+#' ph2Ess(p = c(.8, .2), n=c(6, 30), r=c(4, 11))
+#' ph2Ess(p = c(.8, .2), n = c(18, 18), r = c(12, 11))
 #' @export
-ph2Ess = function( p, n, r) {}
+ph2Ess = function(p, n, r) {}
 
 #' Evaluate the probability that the maximum sample size is needed
 #'
-#' DESCRIPTION HERE
+#' Evaluate the probability that the maximum sample size n1+n2 is
+#' required to complete the trial
 #'
-#' @param p p
-#' @param n n
-#' @param r r
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2) 
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'           and Stage 2 (n2) 
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2)
 #' @examples
-#' # Put exmple code here.
+#' ph2mmax( p = c(.8, .2), n = c(3,33), r = ph2crit(n=c(3,33), p=c(.8, .2), pearly = .1, alpha =.1))
 #' @export
 ph2mmax = function(p, n, r) {}
 
-#' Probability of rejecting the null hypothesis
+#' Probability of rejecting the null hypothesis under traditional sampling
 #'
-#' DESCRIPTION HERE
 #'
-#' @param p p
-#' @param n n
-#' @param r r
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2) 
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'           and Stage 2 (n2) 
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2)
 #' @examples
-#' # Put exmple code here.
-#' @export
+#' ph2reject(p = c( .8, .2), n = c(12, 24), r = c(8, 11))
+#' ph2reject(p = c( .8, .2), n = c(12, 24), r = c(0, 11))
 ph2reject = function(p, n, r) {}
 
-#' Probability of rejecting the null hypothesis under curtail ed sampling
+#' Probability of rejecting the null hypothesis under curtailed sampling
 #'
-#' DESCRIPTION HERE
-#'
-#' @param p p
-#' @param n n
-#' @param r r
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2) 
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'           and Stage 2 (n2) 
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2)
 #' @examples
 #' # Put exmple code here.
 #' @export
@@ -112,13 +134,21 @@ ph2rejcs = function(p, n, r) {}
 
 #' Truncated negative binomial distribution mass function
 #'
-#' DESCRIPTION HERE
+#' Returns the probability density function for the number of events
+#' to reach r= r1 successes in Stage 1 with p = p1 probability of success.
+#' Can also be the number of events to reach r failures with p = (1-p1) probability
+#' of failure.
 #'
-#' @param p p
-#' @param n n
-#' @param r r
+#' @param p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2) or a scalar containing p1
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'           and Stage 2 (n2) or a scalar containing n1
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2) or a scalar containing r1
 #' @examples
-#' # Put exmple code here.
+#' ph2tnb(p = .8, n = 6, r = 4) 
+#` ph2tnb(p = .2, n = 6, r = 3) 
 #' @export
 ph2tnb = function(p, n, r) {}
 
@@ -126,11 +156,16 @@ ph2tnb = function(p, n, r) {}
 #'
 #' DESCRIPTION HERE
 #'
-#' @param p p
-#' @param n n
-#' @param r r
+#' @param p p vector containing the probability of successful outcomes
+#'          in Stage 1 (p1) and Stage 2 (p2)
+#' @param n vector containing sample sizes planned for Stage 1 (n1) 
+#'           and Stage 2 (n2) 
+#' @param r vector containing the minimum number of Stage 1 successes 
+#' to continue to Stage 2 (r1) and the minimum number of Stage 2 
+#' successes to reject the null hypothesis (r2)
 #' @examples
-#' # Put exmple code here.
+#' ph2valid(p = c(.2, .1), n = c(10, 10), r = c(5, 5))
+#' ph2valid(p = c( .2, .3), n = c(10, 10), r = c(5, 5))
 #' @export
 ph2valid = function(p,n,r) {}
 
