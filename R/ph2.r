925428cfe18361d279c1248b283599f05a078cb2
Michael Kane <kaneplusplus@gmail.com>
2017-10-04 15:43:52 -0400

    Fixed a bug Michelle caught.

--- R/ph2.r
+++ R/ph2.r
@@ -184,7 +184,8 @@ dsnb = function(x, prob, s, t) {
   if (t < 1) stop("dsnb t-parameter must be at least 1")
   if (any(prob > 1) || any(prob < 0))
     stop("dsnb prob-parameter must be between zero and one inclusive")
-  apply(dsnb_stacked(x, prob, s, t)[,2:3], 1, sum)
+  browser()
+  apply(dsnb_stacked(x, prob, s, t)[, 2:3, drop=FALSE], 1, sum)
 }
 
 #' @export
